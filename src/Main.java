f34566566680c126930ae230e2095eac386a3c2c
Car-fuentes <carolinainesfuentes@gmail.com>
2025-05-18 21:01:58 -0300

    primer commit

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        // String clave = 8c942c064c65c079d038f6a4
+        //Example Request: https://v6.exchangerate-api.com/v6/8c942c064c65c079d038f6a4/latest/USD
+    }
+}
